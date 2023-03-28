9b3deebd4d0eac1b41dc8ef2cf9edcfc336289e1
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-28 16:34:49 +0300

    Update _nums.c with tabs

--- _nums.c
+++ _nums.c
@@ -8,19 +8,19 @@
 */
 int nums(int j)
 {
-        int n = j;
-        int num_digits = 0;
-        int i, digit;
+	int n = j;
+	int num_digits = 0;
+	int i, digit;
 
-        while (n != 0)
-        {
-                num_digits++;
-                n /= 10;
-        }
-        for (i = num_digits - 1; i >= 0; i--)
-        {
-                digit = (j / (int) pow(10, i)) % 10; // Extract the i-th digit
-                _putchar(digit + '0'); // Output the digit as a character
-        }
-        return (num_digits);
+	while (n != 0)
+	{
+		num_digits++;
+		n /= 10;
+	}
+	for (i = num_digits - 1; i >= 0; i--)
+	{
+		digit = (j / (int) pow(10, i)) % 10; /* Extract the i-th digit */
+		_putchar(digit + '0'); /* Output the digit as a character */
+	}
+	return (num_digits);
 }
