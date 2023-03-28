414a313c67fc7c28803a458f77eb6eee1ec9044d
Hidoreen <doreenwangeci38@gmail.com>
2023-03-28 16:27:05 +0300

    update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,30 +1,5 @@
-#include <unistd.h>
-#include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
-
-/**
- * ord - prints an ordinary character
- * @c: the character to print
- * Return: no of characters printed (1)
-*/
-
-int ord(char c)
-{
-	int count = 0;
-
-	if (c == ' ')
-	{
-		_putchar(' ');
-		count++;
-	}
-	else
-	{
-		_putchar(c);
-		count++;
-	}
-	return (count);
-}
 
 /**
  * _printf - a function that prints output
